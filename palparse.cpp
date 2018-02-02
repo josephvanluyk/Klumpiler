80284369d97020912ebd8bdf5ac2d16e530e0f91
Joseph Van Luyk <jvanluyk21@gmail.com>
2018-02-02 13:52:44 -0600

    Added comments to palparse.cpp

--- palparse.cpp
+++ palparse.cpp
@@ -1,5 +1,15 @@
 #include "scanner.h"
 token tok(0, "", "");
+
+/*
+	One function for each non-terminal
+	If it returns false, that means it failed to find the sequence of tokens to make up its namesake
+	If it returns true, then it parsed properly.
+
+	Example:
+		dcl_definitions() returns true means that if was able to find a "dcl_definitions" in the code
+		mulop() returns false means it couldn't find a mulop.
+*/
 bool pal_program();
 bool dcl_definitions();
 bool procedure();
@@ -15,12 +25,17 @@ bool factor();
 bool more_term();
 bool mulop();
 bool addop();
+
 void error();
+
 int main(){
 	sym = nextSym();
 	tok = getNext();
+
+	//Can we find a pal_program?
 	bool parses = pal_program();
 
+	//If so, we're good. Otherwise, error()
 	if(parses){
 		std::cout << "Successfully parsed." << std::endl;
 	}else{
@@ -29,6 +44,8 @@ int main(){
 }
 
 bool pal_program(){
+		//A pal_program consists of dcl_definitions followed by a procedure.
+			//If we can't find both of those in a row, we don't have a pal_program.
 		if(dcl_definitions()){
 			if(procedure()){
 				return true;
@@ -37,7 +54,10 @@ bool pal_program(){
 		return false;
 }
 
+
 bool dcl_definitions(){
+	//A dcl_definitions is either the dcl lexeme followed by a dcl_list
+		//Or it's nothing. If we see a dcl lexeme but no dcl_list, it's not a dcl_definitions.
 	if(tok.lexeme == "dcl"){
 		tok = getNext();
 		if(dcl_list()){
@@ -51,6 +71,8 @@ bool dcl_definitions(){
 }
 
 bool procedure(){
+	//A procedure is a begin lexeme followed by a statement_list followed by "end" followed by "."
+	//If we don't find all of those things in order, it's not a procedure.
 	if(tok.lexeme == "begin"){
 		tok = getNext();
 		if(statement_list()){
@@ -67,6 +89,9 @@ bool procedure(){
 }
 
 bool dcl_list(){
+	//A dcl_list is an identifier, ";", "int", ";", dcl_list.
+	//Or it's nothing.
+	//If neither of those are the case, it's not a dcl_list.
 	if(tok.tokenName == "Identifier"){
 		tok = getNext();
 		if(tok.lexeme == ":"){
@@ -90,6 +115,9 @@ bool dcl_list(){
 
 
 bool statement_list(){
+	//Statement lists are a statement followed by a statement_list
+	//Or they're nothing
+	//Otherwise, it's not a statement_list
 	if(statement()){
 		if(statement_list()){
 			return true;
@@ -104,6 +132,7 @@ bool statement_list(){
 
 
 bool statement(){
+	//A statement is either an asignment_statement or a write_statement. Otherwise, it's not a statement.
 	if(assignment_statement()){
 		return true;
 	}else if(write_statement()){
@@ -114,6 +143,8 @@ bool statement(){
 }
 
 bool assignment_statement(){
+	//An assignment_statement is an identifier followed by ":=" followed by an expression followd by ";"
+	//If we don't see all those in order, return false.
 	if(tok.tokenName == "Identifier"){
 		tok = getNext();
 		if(tok.lexeme == ":="){
@@ -132,6 +163,8 @@ bool assignment_statement(){
 
 
 bool write_statement(){
+	//A write_statement is "writeln" followed by "(" followed by an identifier followed by ")" followed by ";".
+	//if we don't find all of those in order, return false.
 	if(tok.lexeme == "writeln"){
 		tok = getNext();
 		if(tok.lexeme == "("){
@@ -154,6 +187,8 @@ bool write_statement(){
 
 
 bool expression(){
+	//An expession is a term followed by a more_expression.
+		//If we don't find that in order, return false.
 	if(term()){
 		if(more_expression()){
 			return true;
@@ -164,6 +199,9 @@ bool expression(){
 }
 
 bool more_expression(){
+	//A more_expression is an addop followed by a term followed by a more_expresion
+	//Or it's nothing.
+	//If we a see an addop not followed by a term and expression, return false.
 	if(addop()){
 		if(term()){
 			if(more_expression()){
@@ -177,6 +215,8 @@ bool more_expression(){
 	return true;
 }
 bool term(){
+	//A term is a factor followed by a more_term.
+	//If we don't see those in order, return false.
 	if(factor()){
 		if(more_term()){
 			return true;
@@ -186,6 +226,9 @@ bool term(){
 }
 
 bool more_term(){
+	//A more_term is a mulop followed by a factor followed by a more_term.
+	//Or it's nothing.
+	//If we a see am mulop but not a factor/more_term, return false.
 	if(mulop()){
 		if(factor()){
 			if(more_term()){
@@ -199,6 +242,9 @@ bool more_term(){
 }
 
 bool factor(){
+	//A factor is an identifier or an integer.
+	//Or it's "(" followed by an expression followed by ")".
+	//If none of those work, return false.
 	if(tok.tokenName == "Identifier"){
 		tok = getNext();
 		return true;
@@ -218,6 +264,8 @@ bool factor(){
 	return false;
 }
 
+//An addop is "+" or "-".
+//If we don't find that, return false.
 bool addop(){
 	if(tok.lexeme == "+"){
 		tok = getNext();
@@ -230,6 +278,8 @@ bool addop(){
 	return false;
 }
 
+//A mulop is "*" or "/".
+//If we don't find that, return false.
 bool mulop(){
 	if(tok.lexeme == "*"){
 		tok = getNext();
@@ -240,6 +290,8 @@ bool mulop(){
 	}
 	return false;
 }
+
+//If there's an error, report the line number and where we encountered something unexpected
 void error(){
 	std::cout << "Error on line " << tok.lineNumber << std::endl;
 	std::cout << "Unexpected token: " << tok.lexeme << std::endl;
